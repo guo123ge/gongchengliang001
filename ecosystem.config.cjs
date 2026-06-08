899975ff16e21543081d5d37f4ff0fa021a40365
Developer <>
2026-06-08 21:42:40 +0800

    chore: 调整腾讯云部署端口为3001

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "gongchengliang001",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 3001",
       cwd: "/opt/gongchengliang001",
       env: {
         NODE_ENV: "production",
